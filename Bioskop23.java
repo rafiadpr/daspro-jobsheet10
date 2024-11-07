c3a6cb6c7653a3b056668c8bc819b5da9a6f7d7e
rafiadpr <work.rafiadrianp@gmail.com>
2024-11-07 12:57:46 +0700

    percobaan 1 finish

--- Bioskop23.java
+++ Bioskop23.java
@@ -11,6 +11,33 @@ public class Bioskop23 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
+        // No 5
+        // System.out.println(penonton.length);
+        // for (int i = 0; i < penonton.length; i++) {
+        //     System.out.println("Panjang baris ke-" + (i + 1) + ": " + penonton[i].length);
+        // }
+        System.out.println(penonton.length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+
+        // No 7
+        // System.out.println("Penonton pada baris ke-3: ");
+        // for (int i = 0; i < penonton[2].length; i++) {
+        //     System.out.println(penonton[2][i]);
+        // }
+
+        // No 8
+        // System.out.println("Penonton pada baris ke-3: ");
+        // for (String penontonDiBaris3 : penonton[2]) {
+        //     System.out.println(penontonDiBaris3);
+        // }
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+        }
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
